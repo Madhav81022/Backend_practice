fe32332b6caef13c0ffa55ae65a36d2038d9aed3
Madhav kumar <kumar.madhav810222@gmail.com>
2024-10-06 15:40:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('dotenv').config();
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("Hello Madhav!");
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please login on the console panel</h1>')
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
